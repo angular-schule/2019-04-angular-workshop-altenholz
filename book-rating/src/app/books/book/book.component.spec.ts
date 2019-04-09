052499671bde1b7953eaa4ea10b5b8726384c0f2
Ferdinand Malcher <mail@ferdinand-malcher.de>
2019-04-09 17:12:08 +0200

    Tests BookComponent

--- book-rating/src/app/books/book/book.component.spec.ts
+++ book-rating/src/app/books/book/book.component.spec.ts
@@ -1,8 +1,10 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { BookComponent } from './book.component';
+import { Book } from '../shared/book';
+import { By } from '@angular/platform-browser';
 
-describe('BookComponent', () => {
+fdescribe('BookComponent', () => {
   let component: BookComponent;
   let fixture: ComponentFixture<BookComponent>;
 
@@ -16,10 +18,51 @@ describe('BookComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(BookComponent);
     component = fixture.componentInstance;
+
+    // Initialisierung: VOR detectChanges() !
+    component.book = {
+      isbn: '',
+      title: '',
+      description: '',
+      rating: 3
+    };
+
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should emit event on doRateUp()', () => {
+    // Arrange
+    let eventBook: Book;
+
+    component.rateUp.subscribe(book => {
+      eventBook = book;
+    });
+
+    // Act
+    component.doRateUp();
+
+    // Assert
+    expect(eventBook).toBeTruthy();
+    expect(eventBook).toBe(component.book);
+  });
+
+  it('should call method for button click', () => {
+    // Spionage!
+    spyOn(component, 'doRateUp');
+
+    // Button holen
+    const rateUpBtn = fixture.debugElement
+      .query(By.css('[data-testing-id="rateUpBtn"]'))
+      .nativeElement;
+
+    // Button klicken
+    rateUpBtn.click();
+
+    // gucken ob Methode aufgerufen
+    expect(component.doRateUp).toHaveBeenCalled();
   });
 });
