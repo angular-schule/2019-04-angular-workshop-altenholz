7ffaeebe398d94ada8c42cd2c2fdde5011cece4a
Ferdinand Malcher <mail@ferdinand-malcher.de>
2019-04-11 10:06:23 +0200

    Observables are just Functions

--- book-rating/src/app/app.component.ts
+++ book-rating/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'br-root',
@@ -8,7 +9,33 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Book Rating';
 
-  /*constructor() {
-    setTimeout(() => this.title = 'HALLO', 2000);
-  }*/
+  constructor() {
+    // setTimeout(() => this.title = 'HALLO', 2000);
+
+
+    function producer(obs) {
+      obs.next(1);
+      obs.next(2);
+      obs.complete(3);
+      obs.next(3);
+
+      setTimeout(() => obs.next(4), 2000);
+    }
+
+
+    const observer = {
+      next: value => console.log(value),
+      // error: err => console.log('ERROR', err),
+      // complete: () => console.log('Complete')
+    }
+
+    // producer(observer);
+    // producer(observer);
+
+
+    const myObservable = new Observable(producer);
+    myObservable.subscribe(observer);
+
+
+  }
 }
